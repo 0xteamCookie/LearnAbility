27f7b00a52ac4807ef2c6cbad8c755cacc4e6094
Kathrina-dev <kathrinaelangbam@gmail.com>
2025-03-16 15:18:40 +0530

    added stats on home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -33,7 +33,122 @@ class _HomePageState extends State<HomePage> {
         ],
         ),
           
-        body: Text("This is Home Page")
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              Text(
+                "Welcome Back!",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
+                ),
+              ),
+              
+              Text(
+                "Continue your Learning Journey",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.grey[700],
+                )
+              ),
+              SizedBox(height:20),
+          
+              ElevatedButton(
+                onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const LessonPage(),
+                  ));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal:20.0, vertical: 12.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: Text(
+                  "Continue Learning",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              //Statistics Grid
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 15,
+                  crossAxisSpacing: 15,
+                  childAspectRatio: 3/2,
+                  children: [
+                    _buildStatCard("Study Streak", "7", "days"),
+                     _buildStatCard("Completed Lessons", "24", "lessons"),
+                  _buildStatCard("Weekly Progress", "12.5", "hours"),
+                  _buildStatCard("Quiz Average", "85", "%"),
+                  ],
+                )
+              )
+            ],
+          ),
+        ),
       );
+  }
+
+  //Statcard Widget
+  Widget _buildStatCard(String title, String value, String subtitle){
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withValues(alpha: 0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      padding: EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              Text(
+                value,
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28.0,
+                ),
+              ),
+              SizedBox(width: 5.0),
+              Text(
+                subtitle,
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 14.0
+                ),
+              )
+          ],)
+        ],
+      ),
+    );
   }
 }
