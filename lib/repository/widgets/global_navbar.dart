d048939cf366f54898538945496d6b12bb792fef
0xPixelNinja <rkrabha1858@gmail.com>
2025-03-29 02:09:52 +0530

    remove: rm the voice naviagte for the web platform (no-support)

--- lib/repository/widgets/global_navbar.dart
+++ lib/repository/widgets/global_navbar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lucide_icons/lucide_icons.dart';
-import 'package:my_first_app/ai_voice_page.dart';
 import 'package:my_first_app/videos_page.dart';
 import 'package:provider/provider.dart';
 import '../../Quiz/quizzes_page.dart';
@@ -104,12 +103,6 @@ class _GlobalNavBarState extends State<GlobalNavBar> {
                             title: 'AI Assistant',
                             index: 5,
                             onTap: () => _navigateTo(AIAssistantPage(), 5),
-                          ),
-                          _MenuItem(
-                            icon: LucideIcons.brain,
-                            title: 'AI Voice Assistant',
-                            index: 6,
-                            onTap: () => _navigateTo(VoiceAiChat(), 6),
                           ),
                         ],
                         settings,
